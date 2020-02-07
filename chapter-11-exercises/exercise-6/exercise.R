ddeadc38acd0816817fda9760a742eeac2c87345
Steven Lu <hsuelu@uw.edu>
2020-02-07 14:55:03 -0800

    add Week 5 exercise

--- chapter-11-exercises/exercise-6/exercise.R
+++ chapter-11-exercises/exercise-6/exercise.R
@@ -5,14 +5,24 @@
 #install.packages("nycflights13")  # should be done already
 library("nycflights13")
 library("dplyr")
-
+View(airports)
 # Create a dataframe of the average arrival delays for each _destination_, then 
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
-
-
+flights %>% 
+  group_by(dest) %>% 
+  summarize(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>% 
+  mutate(faa = dest) %>% 
+  left_join(airports, by = "faa") %>% 
+  filter(avg_arr_delay == max(avg_arr_delay, na.rm = TRUE))
+  
+  
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
 # Which airline had the smallest average arrival delay?
-
+flights %>% 
+  group_by(carrier) %>% 
+  summarize(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>% 
+  left_join(airlines, by = "carrier") %>% 
+  arrange(-avg_arr_delay)
